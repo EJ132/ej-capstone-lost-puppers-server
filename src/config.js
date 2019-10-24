5978635e7b862c12063bb8077e103a1e185d51a0
EJ <ejg132@gmail.com>
2019-10-24 13:44:27 -0700

    hidden files

--- src/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://ejgonzalez@localhost/lost-puppers',
-    JWT_SECRET: process.env.JWT_SECRET || 'Raiders1',
-    ACCESS_KEY_ID: 'AKIAJ2FN6KCPGUANVWPQ',
-    SECRET_ACCESS_KEY: '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',
-  }
