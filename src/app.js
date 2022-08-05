603359e899e40349bd6fa0bb3422f2ecd9e2af2d
EJ132 <ejg132@gmail.com>
2022-08-04 21:02:39 -0700

    updating packages and linter

--- src/app.js
+++ src/app.js
@@ -1,43 +1,45 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
-const PupsRouter = require('./pups/pups-router')
-const AuthRouter = require('./auth/auth-router')
-const UserRouter = require('./users/user-router')
-const ProfileRouter = require('./profile/profile-router')
-const CommentRouter = require('./comments/comment-router')
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const { NODE_ENV } = require("./config");
+const PupsRouter = require("./pups/pups-router");
+const AuthRouter = require("./auth/auth-router");
+const UserRouter = require("./users/user-router");
+const ProfileRouter = require("./profile/profile-router");
+const CommentRouter = require("./comments/comment-router");
 
-const app = express()
+const app = express();
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
-app.use(morgan(morganOption))
-app.use(cors())
-app.use(helmet())
+app.use(morgan(morganOption));
+app.use(cors());
+app.use(helmet());
 
-app.use('/uploads', express.static('uploads'))
+app.get("/", (req, res) => {
+  res.send("Lost Puppers API is running!");
+});
 
-app.use('/api/pups', PupsRouter)
-app.use('/api/auth', AuthRouter)
-app.use('/api/users', UserRouter)
-app.use('/api/profile', ProfileRouter)
-app.use('/api/comments', CommentRouter)
+app.use("/uploads", express.static("uploads"));
+
+app.use("/api/pups", PupsRouter);
+app.use("/api/auth", AuthRouter);
+app.use("/api/users", UserRouter);
+app.use("/api/profile", ProfileRouter);
+app.use("/api/comments", CommentRouter);
 
 app.use(function errorHandler(error, req, res, next) {
-       let response
-       if (NODE_ENV === 'production') {
-         console.log(error)
-         response = { error: { message: 'server error' } }
-       } else {
-         console.error(error)
-         response = { message: error.message, error }
-       }
-       res.status(500).json(response)
-     })
+  let response;
+  if (NODE_ENV === "production") {
+    console.log(error);
+    response = { error: { message: "server error" } };
+  } else {
+    console.error(error);
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+});
 
-module.exports = app
+module.exports = app;
