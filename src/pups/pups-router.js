7b001467bb8b318a643b851fa070e8f9bea478d4
EJ132 <ejg132@gmail.com>
2022-08-19 00:58:16 -0700

    fxing up BE

--- src/pups/pups-router.js
+++ src/pups/pups-router.js
@@ -1,118 +1,112 @@
-const express = require('express')
-const PupsService = require('./pups-service')
-const {requireAuth} = require('../middleware/jwt-auth')
-const jsonBodyParser = express.json()
+const express = require("express");
+const PupsService = require("./pups-service");
+const { requireAuth } = require("../middleware/jwt-auth");
+const jsonBodyParser = express.json();
 
-const upload = require('../file-upload')
+const upload = require("../file-upload");
 
-
-const PupsRouter = express.Router()
-
-PupsRouter
-  .all(requireAuth)
-  .post('/', upload.single('image'), (req, res, next) => {
-    const image = req.file.location
-    const {name, lat, long, description, category, zipcode, owner } = req.body
-    const newPup = {name, image, description, lat, long, category, zipcode, owner }
-    for (const field of ['name', 'lat', 'long', 'description', 'category', 'zipcode', 'owner'])
-       if (!req.body[field])
-         return res.status(400).json({
-           error: `Missing '${field}' in request body`
-         })
-         
-    PupsService.insertPup(
-      req.app.get('db'),
-      newPup
-    )
-      .then(Pup => {
-        res
-          .status(201)
-          .json(PupsService.serializePup(Pup))
-      })
-      .catch(err => console.log(err))
-
-  })
-  .get('/',(req, res, next) => {
-    PupsService.getAllPups(req.app.get('db'))
-      .then(pups => {
-        res.json(PupsService.serializePups(pups))
-      })
-      .catch(next)
-  })
+const PupsRouter = express.Router();
 
 PupsRouter
-  .route('/:id')
+  // .all(requireAuth)
+  .post("/", upload.single("image"), (req, res, next) => {
+    const image = req.file.location;
+    const { name, lat, long, description, category, zipcode, owner } = req.body;
+    const newPup = {
+      name,
+      image,
+      description,
+      lat,
+      long,
+      category,
+      zipcode,
+      owner,
+    };
+    for (const field of [
+      "name",
+      "lat",
+      "long",
+      "description",
+      "category",
+      "zipcode",
+      "owner",
+    ])
+      if (!req.body[field])
+        return res.status(400).json({
+          error: `Missing '${field}' in request body`,
+        });
+
+    PupsService.insertPup(req.app.get("db"), newPup)
+      .then((Pup) => {
+        res.status(201).json(PupsService.serializePup(Pup));
+      })
+      .catch((err) => console.log(err));
+  })
+  .get("/", (req, res, next) => {
+    PupsService.getAllPups(req.app.get("db"))
+      .then((pups) => {
+        res.json(PupsService.serializePups(pups));
+      })
+      .catch(next);
+  });
+
+PupsRouter.route("/:id")
   .all(requireAuth)
   .all(checkPupExists)
   .get((req, res) => {
-    res.json(PupsService.serializePup(res.thing))
+    res.json(PupsService.serializePup(res.thing));
   })
   .delete((req, res, next) => {
-    PupsService.deletePup(
-      req.app.get('db'),
-      req.params.id
-    )
-      .then(numRowsAffected => {
-        res.status(204).end()
+    PupsService.deletePup(req.app.get("db"), req.params.id)
+      .then((numRowsAffected) => {
+        res.status(204).end();
       })
-      .catch(next)
+      .catch(next);
   })
   .patch(jsonBodyParser, (req, res, next) => {
-    const { name, description } = req.body
-    const pupToUpdate = { name, description }
+    const { name, description } = req.body;
+    const pupToUpdate = { name, description };
 
-    const numberOfValues = Object.values(pupToUpdate).filter(Boolean).length
+    const numberOfValues = Object.values(pupToUpdate).filter(Boolean).length;
     if (numberOfValues === 0)
       return res.status(400).json({
         error: {
-          message: `Request body must content either 'name' or 'description'`
-        }
-      })
+          message: `Request body must content either 'name' or 'description'`,
+        },
+      });
 
-    PupsService.updatePup(
-      req.app.get('db'),
-      req.params.id,
-      pupToUpdate
-    )
-      .then(updatedPup => {
-        res.status(200).json(updatedPup)
+    PupsService.updatePup(req.app.get("db"), req.params.id, pupToUpdate)
+      .then((updatedPup) => {
+        res.status(200).json(updatedPup);
       })
-      .catch(next)
-  })
+      .catch(next);
+  });
 
-PupsRouter
-  .route('/:id/comments')
+PupsRouter.route("/:id/comments")
   .all(requireAuth)
   .get((req, res, next) => {
-    PupsService.getAllComments(
-      req.app.get('db'),
-      req.params.id
-    )
+    PupsService.getAllComments(req.app.get("db"), req.params.id)
 
-    .then(comments => {
-      res.json(PupsService.serializePupComments(comments))
-    })
-    .catch(next)
-  })
-
+      .then((comments) => {
+        res.json(PupsService.serializePupComments(comments));
+      })
+      .catch(next);
+  });
 
 /* async/await syntax for promises */
 async function checkPupExists(req, res, next) {
   try {
-    const thing = await PupsService.getById(
-      req.app.get('db'),
-      req.params.id
-    )
+    const thing = await PupsService.getById(req.app.get("db"), req.params.id);
 
     if (!thing)
       return res.status(404).json({
-        error: `Pup doesn't exist`
-      })
+        error: `Pup doesn't exist`,
+      });
 
-    res.thing = thing
-    next()
+    res.thing = thing;
+    next();
   } catch (error) {
-    next(error)
+    next(error);
   }
 }
 
